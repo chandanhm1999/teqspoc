0b46180cb7375daec57f8551e44ddc3a60cb1e1c
chandan hm <chandanhm388@gmail.com>
2024-10-17 23:02:41 +0530

    second commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
@@ -7,10 +7,22 @@ import "react-multi-carousel/lib/styles.css";
 import Footer from "@/components/footer/Footer";
 import WhatsApp from "@/components/whatsapp/whatsapp";
 import { metadata } from "./layoutMetadata";
+import Preloader from "@/components/preloader/Preloader";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading completion after 3 seconds
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <html lang="en">
       <title>{metadata.title}</title>
@@ -53,7 +65,8 @@ export default function RootLayout({ children }) {
     <meta name="rating" content={metadata.rating} />
     <meta name="viewport" content={metadata.viewport} />
       <body>
-        <div className="">
+      {loading && <Preloader />} {/* Show loader if loading state is true */}
+      <div className={loading ? "hidden" : "visible"}>
         <Navbar /> 
         {children}
         <WhatsApp />
